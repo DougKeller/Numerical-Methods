88e075b1f44e84b423f9e87bdc0d74b78b637aa8
DougKeller <dwk24@zips.uakron.edu>
2016-09-07 23:37:59 -0400

    Use proper addition/subtraction action depending on sign of b in q6

--- HW1/quadform.m
+++ HW1/quadform.m
@@ -2,10 +2,10 @@ function [xp, xm] = quadform(a, b, c)
     discriminant = b * b - 4 * a * c;
     
     if (b < 0)
-        xp = b * -1 - sqrt(discriminant);
+        xp = b * -1 + sqrt(discriminant);
         xm = c / (2 * xp);
     else
-        xm = b * -1 + sqrt(discriminant);
+        xm = b * -1 - sqrt(discriminant);
         xp = c / (2 * xm);
     end
 end
