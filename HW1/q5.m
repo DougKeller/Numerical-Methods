86ef0a3a6f3f782cba0b3933bfa19436672890f6
DougKeller <dwk24@zips.uakron.edu>
2016-09-07 23:30:53 -0400

    Correct sign on q5

--- HW1/q5.m
+++ HW1/q5.m
@@ -8,7 +8,7 @@ x = -8323.6;
 fprintf('f(%6.8f) = %6.8f \n', x, f(x));
 fprintf('f(%6.10e) = %6.10e \n\n', x, f(x));
 
-x = -0.0003;
+x = 0.0003;
 fprintf('f(%6.8f) = %6.8f \n', x, f(x));
 fprintf('f(%6.10e) = %6.10e \n\n', x, f(x));
 
